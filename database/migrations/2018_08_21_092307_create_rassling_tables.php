0e81d12c916e04837922da46a274b5f15a4dab11
vpj <vpjpeltonen@gmail.com>
2018-08-22 19:50:55 +0300

    fixed migration

--- database/migrations/2018_08_21_092307_create_rassling_tables.php
+++ database/migrations/2018_08_21_092307_create_rassling_tables.php
@@ -17,87 +17,87 @@ class CreateRasslingTables extends Migration
         Schema::create('wrestlers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('height');
-            $table->integer('weight');
-            $table->date('birth');
-            $table->char('gender');
+            $table->integer('height')->nullable($value = true);
+            $table->integer('weight')->nullable($value = true);
+            $table->date('birth')->nullable($value = true);
+            $table->char('gender')->nullable($value = true);
         });
         //championships
         Schema::create('championships', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('promotion');
+            $table->string('promotion')->nullable($value = true);
         });
         //tag teams
         Schema::create('tag_teams', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('wrestler1');
-            $table->integer('wrestler2');
-            $table->foreign('wrestler1')->references('id')->on('wrestlers');
-            $table->foreign('wrestler2')->references('id')->on('wrestlers');
+            $table->string('wrestler1');
+            $table->string('wrestler2');
+            $table->foreign('wrestler1')->references('name')->on('wrestlers');
+            $table->foreign('wrestler2')->references('name')->on('wrestlers');
         });        
         //times wrestlers held the championships
         Schema::create('championship_reigns', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('wrestler');
-            $table->integer('championship');
-            $table->date('begining');
-            $table->date('end');
-            $table->foreign('wrestler')->references('id')->on('wrestlers');
-            $table->foreign('championship')->references('id')->on('championships');
+            $table->string('wrestler');
+            $table->string('championship');
+            $table->date('begining')->nullable($value = true);
+            $table->date('end')->nullable($value = true);
+            $table->foreign('wrestler')->references('name')->on('wrestlers');
+            $table->foreign('championship')->references('name')->on('championships');
         });  
         //times tag teams have held championships
         Schema::create('tag_reigns', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('tag_team');
-            $table->integer('championship');
-            $table->date('begining');
-            $table->date('end');
-            $table->foreign('tag_team')->references('id')->on('tag_teams');
-            $table->foreign('championship')->references('id')->on('championships');
+            $table->string('tag_team');
+            $table->string('championship');
+            $table->date('begining')->nullable($value = true);
+            $table->date('end')->nullable($value = true);
+            $table->foreign('tag_team')->references('name')->on('tag_teams');
+            $table->foreign('championship')->references('name')->on('championships');
         });   
         //singles match results     
         Schema::create('singles_results', function (Blueprint $table) {
             $table->increments('id');
             $table->string('result');
-            $table->integer('winner');
-            $table->integer('championship');
-            $table->boolean('championship_change');
-            $table->time('length');
-            $table->date('date');
-            $table->string('promotion');
-            $table->foreign('winner')->references('id')->on('wrestlers');
-            $table->foreign('championship')->references('id')->on('championships');
+            $table->string('winner')->nullable($value = true);
+            $table->string('championship')->nullable($value = true);
+            $table->boolean('championship_change')->nullable($value = true);
+            $table->time('length')->nullable($value = true);
+            $table->date('date')->nullable($value = true);
+            $table->string('promotion')->nullable($value = true);
+            $table->foreign('winner')->references('name')->on('wrestlers');
+            $table->foreign('championship')->references('name')->on('championships');
         });    
         //singles match participants
         Schema::create('singles_participations', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('wrestler');
-            $table->integer('match');
-            $table->foreign('wrestler')->references('id')->on('wrestlers');
-            $table->foreign('match')->references('id')->on('singles_results');
+            $table->string('wrestler');
+            $table->string('contest');
+            $table->foreign('wrestler')->references('name')->on('wrestlers');
+            $table->foreign('contest')->references('id')->on('singles_results');
         });    
         //tag matches
         Schema::create('tag_results', function (Blueprint $table) {
             $table->increments('id');
             $table->string('result');
-            $table->integer('winner');
-            $table->integer('championship');
-            $table->boolean('championship_change');
-            $table->time('length');
-            $table->date('date');
-            $table->string('promotion');
-            $table->foreign('winner')->references('id')->on('tag_teams');
-            $table->foreign('championship')->references('id')->on('championships');
+            $table->string('winner')->nullable($value = true);
+            $table->string('championship')->nullable($value = true);
+            $table->boolean('championship_change')->nullable($value = true);
+            $table->time('length')->nullable($value = true);
+            $table->date('date')->nullable($value = true);
+            $table->string('promotion')->nullable($value = true);
+            $table->foreign('winner')->references('name')->on('tag_teams');
+            $table->foreign('championship')->references('name')->on('championships');
         });  
         //tag team match participants  
         Schema::create('tag_participations', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('team');
-            $table->integer('match');
-            $table->foreign('team')->references('id')->on('tag_teams');
-            $table->foreign('match')->references('id')->on('tag_results');
+            $table->string('team');
+            $table->string('contest');
+            $table->foreign('team')->references('name')->on('tag_teams');
+            $table->foreign('contest')->references('id')->on('tag_results');
         });                                    
     }
 
