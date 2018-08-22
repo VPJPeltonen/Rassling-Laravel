0876aea842f6daa65b316cdfe406ad4877b0bfe1
vpj <vpjpeltonen@gmail.com>
2018-08-22 15:14:17 +0300

    fixed my horrible migration

--- database/migrations/2018_08_21_092307_create_rassling_tables.php
+++ database/migrations/2018_08_21_092307_create_rassling_tables.php
@@ -17,10 +17,10 @@ class CreateRasslingTables extends Migration
         Schema::create('wrestlers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->int('height');
-            $table->int('weight');
-            $table->date('birth')
-            $table->char('gender')
+            $table->integer('height');
+            $table->integer('weight');
+            $table->date('birth');
+            $table->char('gender');
         });
         //championships
         Schema::create('championships', function (Blueprint $table) {
@@ -32,37 +32,37 @@ class CreateRasslingTables extends Migration
         Schema::create('tag_teams', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->int('wrestler1');
-            $table->int('wrestler2');
+            $table->integer('wrestler1');
+            $table->integer('wrestler2');
             $table->foreign('wrestler1')->references('id')->on('wrestlers');
             $table->foreign('wrestler2')->references('id')->on('wrestlers');
         });        
         //times wrestlers held the championships
         Schema::create('championship_reigns', function (Blueprint $table) {
             $table->increments('id');
-            $table->int('wrestler');
-            $table->int('championship');
+            $table->integer('wrestler');
+            $table->integer('championship');
             $table->date('begining');
             $table->date('end');
             $table->foreign('wrestler')->references('id')->on('wrestlers');
             $table->foreign('championship')->references('id')->on('championships');
         });  
         //times tag teams have held championships
-        Schema::create('championship_reigns', function (Blueprint $table) {
+        Schema::create('tag_reigns', function (Blueprint $table) {
             $table->increments('id');
-            $table->int('tag_team');
-            $table->int('championship');
+            $table->integer('tag_team');
+            $table->integer('championship');
             $table->date('begining');
             $table->date('end');
-            $table->foreign('wrestler')->references('id')->on('wrestlers');
+            $table->foreign('tag_team')->references('id')->on('tag_teams');
             $table->foreign('championship')->references('id')->on('championships');
         });   
         //singles match results     
         Schema::create('singles_results', function (Blueprint $table) {
             $table->increments('id');
             $table->string('result');
-            $table->int('winner');
-            $table->int('championship');
+            $table->integer('winner');
+            $table->integer('championship');
             $table->boolean('championship_change');
             $table->time('length');
             $table->date('date');
@@ -73,8 +73,8 @@ class CreateRasslingTables extends Migration
         //singles match participants
         Schema::create('singles_participations', function (Blueprint $table) {
             $table->increments('id');
-            $table->int('wrestler');
-            $table->int('match');
+            $table->integer('wrestler');
+            $table->integer('match');
             $table->foreign('wrestler')->references('id')->on('wrestlers');
             $table->foreign('match')->references('id')->on('singles_results');
         });    
@@ -82,8 +82,8 @@ class CreateRasslingTables extends Migration
         Schema::create('tag_results', function (Blueprint $table) {
             $table->increments('id');
             $table->string('result');
-            $table->int('winner');
-            $table->int('championship');
+            $table->integer('winner');
+            $table->integer('championship');
             $table->boolean('championship_change');
             $table->time('length');
             $table->date('date');
@@ -91,12 +91,12 @@ class CreateRasslingTables extends Migration
             $table->foreign('winner')->references('id')->on('tag_teams');
             $table->foreign('championship')->references('id')->on('championships');
         });  
-        //tag team participants  
-        Schema::create('tag', function (Blueprint $table) {
+        //tag team match participants  
+        Schema::create('tag_participations', function (Blueprint $table) {
             $table->increments('id');
-            $table->int('team');
-            $table->int('match');
-            $table->foreign('wrestler')->references('id')->on('tag_teams');
+            $table->integer('team');
+            $table->integer('match');
+            $table->foreign('team')->references('id')->on('tag_teams');
             $table->foreign('match')->references('id')->on('tag_results');
         });                                    
     }
