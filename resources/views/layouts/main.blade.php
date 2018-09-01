453285b119da06ead595a0be77f151f55dd9cd82
VPJP <vpjpeltonen@gmail.com>
2018-09-01 19:47:46 +0300

    played around with layouts and views

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rassler</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,11 @@
+<style>
+#navi li {
+    display: inline;
+}
+#navi{
+  background-color: black;
+}
+#content {
+    background-color: lightblue;
+}
+</style>
