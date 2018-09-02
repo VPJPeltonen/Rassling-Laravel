1a7af6dca7266cf0762388511c9c832822467ffa
VPJP <vpjpeltonen@gmail.com>
2018-09-02 16:21:23 +0300

    added some basic structure for different parts of page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,8 +2,17 @@
 <html>
 <head>
   <title>Rassler</title>
+  @include('layouts.style')
 </head>
 <body>
-  @yield('content')
+  <div id='navi'>
+    @include('layouts.navbar')
+  </div>
+  <div id='content'>
+    @yield('content')
+  </div>
+  <div id='footer'>
+    @yield('footer')
+  </div>
 </body>
 </html>
