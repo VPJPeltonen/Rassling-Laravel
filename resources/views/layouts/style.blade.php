39d60dbe1f73de30a879d6fbae6610a523f3f3f7
VPJP <vpjpeltonen@gmail.com>
2018-09-02 21:13:32 +0300

    got matches working and made it all look less ugly

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -1,11 +1,45 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
+a{
+  text-decoration: none;
+  color: #EAC67A;
+}
+body {
+  margin: auto;
+  width: 95%;
+  padding: 5px;
+  border: 0px solid gray;
+  background-color: #18121E;
+}
+#navi, #footer{
+  background-color: #233237;
+  color: white;
+}
 #navi li {
     display: inline;
 }
 #navi{
-  background-color: black;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  position: fixed;
+  top: 0;
+  height: 50px;
+  width: 95%;
 }
 #content {
-    background-color: lightblue;
+    background-color: #984B34;
+    color: black;
+    top: 55px;
+    position: fixed;
+    width: 95%;
+    height: 90%;
+}
+#content a{
+  color: black;
+}
+#footer{
+  position: fixed;
+  bottom: 0;
+  width: 95%;
 }
 </style>
